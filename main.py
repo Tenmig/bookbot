b8fc0e214ce0270f768391e8e067c547b33f56a8
ThePrimeagen <the.primeagen@aol.com>
2025-10-15 10:25:59 +0200

    D: added conut_words function

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ def get_book_text(filepath):
         book_text = f.read()
     return str(book_text)
 
+def count_words(book_text):
+    words = book_text.split()
+    words_count = len(words)
+    return words_count
+
 def main():
-    print(get_book_text("books/frankenstein.txt"))
+    words_count = count_words(get_book_text("books/frankenstein.txt"))
+    print(f"Found {words_count} total words")
 
 main()
